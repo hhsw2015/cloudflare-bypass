35840ac5d489a9b41b73927de955a280917612f6
wowdd1 <developergf@gmail.com>
2025-07-08 19:00:56 +0800

    add

--- install_tools.md
+++
@@ -1,9 +0,0 @@
-# 容器工具安装指令
-
-在Firefox容器中安装xdotool工具：
-
-```bash
-docker exec firefox sh -c "apk update && apk add xdotool"
-```
-
-就这一条命令即可。
--- install_tools_in_container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# 在Firefox容器中安装必要工具
-
-echo "在Firefox容器中安装xdotool..."
-docker exec firefox sh -c "apk update && apk add xdotool"
-echo "工具安装完成！"
